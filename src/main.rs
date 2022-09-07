e3b932646a2be269878753a8fec0e7c1faaf4cf4
frostornge <frostornge@gmail.com>
2022-09-07 20:58:40 +0900

    chores

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use std::env::current_dir;
-use std::fs::create_dir_all;
-
 use clap::Parser;
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use std::env::current_dir;
+use std::fs::create_dir_all;
 
 #[derive(clap::Parser)] // requires `derive` feature
 #[clap(author, version, about, long_about = None)]
@@ -18,6 +17,7 @@ fn main() {
     match Cli::parse() {
         Cli::Schema(_) => {
             create_core_schemas();
+            create_market_schemas();
             create_oracle_schemas();
             create_treasury_schemas();
         }
@@ -25,8 +25,7 @@ fn main() {
 }
 
 fn create_core_schemas() {
-    use noi_core::msg::{CustomResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-    use noi_core::state::State;
+    use noi_interface::core::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema/core");
@@ -36,13 +35,10 @@ fn create_core_schemas() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
-    export_schema(&schema_for!(CustomResponse), &out_dir);
 }
 
-fn create_oracle_schemas() {
-    use noi_oracle::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
-    use noi_oracle::state::State;
+fn create_market_schemas() {
+    use noi_interface::market::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema/oracle");
@@ -52,12 +48,23 @@ fn create_oracle_schemas() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(State), &out_dir);
+}
+
+fn create_oracle_schemas() {
+    use noi_interface::oracle::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+    let mut out_dir = current_dir().unwrap();
+    out_dir.push("schema/oracle");
+    create_dir_all(&out_dir).unwrap();
+    remove_schemas(&out_dir).unwrap();
+
+    export_schema(&schema_for!(InstantiateMsg), &out_dir);
+    export_schema(&schema_for!(ExecuteMsg), &out_dir);
+    export_schema(&schema_for!(QueryMsg), &out_dir);
 }
 
 fn create_treasury_schemas() {
-    use noi_treasury::msg::{ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-    use noi_treasury::state::Config;
+    use noi_interface::treasury::{ConfigResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
 
     let mut out_dir = current_dir().unwrap();
     out_dir.push("schema/treasury");
@@ -67,6 +74,5 @@ fn create_treasury_schemas() {
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
-    export_schema(&schema_for!(Config), &out_dir);
     export_schema(&schema_for!(ConfigResponse), &out_dir);
 }
